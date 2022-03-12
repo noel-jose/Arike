fb4ab961caa2d08d4da034ebe2a694ac51223e99
noel-jose <noeljosetp@gmail.com>
2022-03-12 21:13:14 +0530

    minor changes

--- arike/settings.py
+++ arike/settings.py
@@ -87,12 +87,12 @@ WSGI_APPLICATION = "arike.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
-}
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": BASE_DIR / "db.sqlite3",
+#     }
+# }
 
 DATABASES = {
     "default": env.db(
